9bb40663c897bf2529f06cae9c0a240796446038
Hassan Tahseen <ht3aa2001@gmail.com>
2022-04-23 21:08:15 +0300

    aa

--- data/db.js
+++ data/db.js
@@ -5,7 +5,7 @@ export const db = {
             lesson_name: "المحاضرة الأولى",
             lesson_number: "#0",
             description: "مقدمة عن برنامج الأكسل. أنواع أستخدامات الشائعة للأكسل. قوة أستخدام الأكسل. و غيرها الكثير",
-            file_path: "Introduction",
+            file_path: "Introduction_excel",
             video_link: "https://www.youtube.com/embed/6yUKy-XLhyc",
         },
         {
@@ -13,7 +13,7 @@ export const db = {
             lesson_name: "المحاضرة الثانية",
             lesson_number: "#1",
             description: "في هذه المحاضرة سيتم شرح كيفية فتح ملف أكسل و ما هو الفرق بين الحفظ و الحفظ بأسم",
-            file_path: "Lecture_1",
+            file_path: "Lecture_1_excel",
             video_link: "https://www.youtube.com/embed/EETFoj9v874",
         },
         {
@@ -21,7 +21,7 @@ export const db = {
             lesson_name: "المحاضرة الثالثة",
             lesson_number: "#2",
             description: "في هذه المحاضرة سنتكلم عن الدوال: الحمع, المعدل, أصغر قيمة, أكبر قيمة, كبير, صغير",
-            file_path: "Lecture_2",
+            file_path: "Lecture_2_excel",
             video_link: "https://www.youtube.com/embed/1PtrtmHey8M",
         },
         {
@@ -29,7 +29,7 @@ export const db = {
             lesson_name: "المحاضرة الرابعة الجزء الأول",
             lesson_number: "#3",
             description: "في هذه المحاضرة سنتكلم عن الدوال الشرطية و العوامل المنطقية. الجزء الأول",
-            file_path: "Lecture_3",
+            file_path: "Lecture_3_excel",
             video_link: "https://www.youtube.com/embed/cacUxoBkmsc",
         },
         {
@@ -37,7 +37,7 @@ export const db = {
             lesson_name: "المحاضرة الرابعة الجزء الثاني",
             lesson_number: "#4",
             description: "في هذه المحاضرة سنتكلم عن الدوال الشرطية و العوامل المنطقية. الجزء الثاني",
-            file_path: "Lecture_3",
+            file_path: "Lecture_3_excel",
             video_link: "https://www.youtube.com/embed/CzkSw0RcdAc",
         },
         {
@@ -45,7 +45,7 @@ export const db = {
             lesson_name: "المحاضرة الرابعة الجزء الثالث",
             lesson_number: "#5",
             description: "في هذه المحاضرة سنتكلم عن الدوال الشرطية و العوامل المنطقية. الجزء الثالث",
-            file_path: "Lecture_3",
+            file_path: "Lecture_3_excel",
             video_link: "https://www.youtube.com/embed/rCY6oQRnUDE",
         },
         {
@@ -53,20 +53,9 @@ export const db = {
             lesson_name: "المحاضرة الرابعة الجزء الرابع",
             lesson_number: "#6",
             description: "في هذه المحاضرة سنتكلم عن الدوال الشرطية و العوامل المنطقية. الجزء الرابع",
-            file_path: "Lecture_3",
+            file_path: "Lecture_3_excel",
             video_link: "https://www.youtube.com/embed/VG2wrSyYhDw",
         },
 
     ]
 }
-
-// 1: https://youtu.be/6yUKy-XLhyc
-
-// 2: https://youtu.be/EETFoj9v874
-
-// 3: https://youtu.be/1PtrtmHey8M
-
-// 4 part 1: https://youtu.be/cacUxoBkmsc
-// 4 part 2: https://youtu.be/CzkSw0RcdAc
-// 4 part 3: https://youtu.be/rCY6oQRnUDE
-// 4 part 4: https://youtu.be/VG2wrSyYhDw
